bb5392e06c708da2e16bf5027b26072d3b34576c
omearamike <mikeytoomo@hotmail.com>
2016-12-22 11:26:09 +0000

    first commit

---
+++ DatabaseConnection.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+use \PDO;
+use \PDOException;
+
+
+class DatabaseConnection extends PDO
+{
+    private $user = 'farmadmin';
+    private $password = 'mike';
+    private $host = 'localhost';
+    private $port = 3360;
+    private $dbname = 'farm_System';
+
+    private $dsn;
+
+    public function __construct()
+    {
+        $this->dsn = 'mysql:dbname=' . $this->dbname . ';host=' . $this->host . ';port=' . $this->port . '';
+        try {
+            parent::__construct($this->dsn, $this->user, $this->password);
+        } catch (PDOException $e) {
+            echo 'Connection failed: ' . $e->getMessage();
+        }
+    }
+
+    public static function make()
+    {
+        return new self();
+    }
+
+}
---
+++ getallentries.php
@@ -0,0 +1,5 @@
+<?php
+namespace App;
+require_once __DIR__ . '/Test.php';
+
+    var_dump (Test::make()->getAllTestEntries());
