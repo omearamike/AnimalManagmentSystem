fb3d3ce31538c7152b85a671b70b2ff7f658c406
omearamike <mikeytoomo@hotmail.com>
2016-12-22 12:40:26 +0000

    - Organised File structure - Implemented Basic Templating Structure

---
+++ getallentriesFunc.php
@@ -0,0 +1,5 @@
+<?php
+namespace App;
+require_once __DIR__ . '/Functions.php';
+
+    var_dump (Functions::make()->getAllTestEntries());
---
+++ include/header.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
+      <link href="css/bootstrap.min.css" rel="stylesheet">
+      <link href="css/style.css" rel="stylesheet">
+
+    </head>
+
+
+    <body>
+  <div class="container-fluid">
+  	<div class="row">
+  		<div class="col-md-12">
+  			<ul class="nav nav-tabs">
+  				<li class="active">
+  					<a href="index.html">Home</a>
+  				</li>
+  				<li>
+  					<a href="create.php">Create</a>
+  				</li>
+          <li>
+            <a href="update.php">Update</a>
+          </li>
+  				<li class="disabled">
+  					<a href="#">Messages</a>
+  				</li>
+  				<li class="dropdown pull-right">
+  					 <a href="#" data-toggle="dropdown" class="dropdown-toggle">Dropdown<strong class="caret"></strong></a>
+  					<ul class="dropdown-menu">
+  						<li>
+  							<a href="#">Action</a>
+  						</li>
+  						<li>
+  							<a href="#">Another action</a>
+  						</li>
+  						<li>
+  							<a href="#">Something else here</a>
+  						</li>
+  						<li class="divider">
+  						</li>
+  						<li>
+  							<a href="#">Separated link</a>
+  						</li>
+  					</ul>
+  				</li>
+  			</ul>
+      </div>
+    </div>
+  </div>
